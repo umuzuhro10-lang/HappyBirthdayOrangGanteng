563d52210186c95f5334432884901998b8952237
zizi <umuzuhro10@gmail.com>
2026-06-28 08:38:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const welcome = document.getElementById("welcome");
 const giftPage = document.getElementById("giftPage");
-const wishPage = document.getElementById("wishPage");
 const cakePage = document.getElementById("cakePage");
 const letterPage = document.getElementById("letterPage");
 
 const giftBtn = document.getElementById("giftBtn");
 const blowBtn = document.getElementById("blowBtn");
 const typing = document.getElementById("typing");
-
 
 giftBtn.onclick = () => {
     welcome.classList.add("hidden");
@@ -19,39 +17,12 @@ giftBtn.onclick = () => {
     }, 2500);
 };
 
-const flame = document.getElementById("flame");
-
-const wishBtn = document.getElementById("wishBtn");
-
 blowBtn.onclick = () => {
-
-    flame.style.opacity = "0";
-
-    setTimeout(() => {
-
-        cakePage.classList.add("hidden");
-        wishPage.classList.remove("hidden");
-
-    },1500);
-
-}
-const wishBtn = document.getElementById("wishBtn");
-
-wishBtn.onclick = () => {
-  const input = document.getElementById("wishInput");
-  const result = document.getElementById("wishResult");
-
-  const wish = input.value;
-
-  if (wish === "") {
-    result.innerText = "Tulis wish dulu ya 😊";
-    return;
-  }
-
-  result.innerText = "✨ Wish kamu: " + wish;
-
-  input.value = "";
+    cakePage.classList.add("hidden");
+    letterPage.classList.remove("hidden");
+    typeMessage();
 };
+
 const message =
 `Haiii, Happy Birthday yaa! 🥳🎂💛
 
